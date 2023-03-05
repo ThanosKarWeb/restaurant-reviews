dd0319430ca405289b7c19d6f7847711559246e3
ThanosKarWeb <thanos.kar.web@gmail.com>
2023-03-05 20:59:22 +0200

    RestRev basic functionality

---
+++ frontend/src/components/add-review.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddReview() {
+  return <div className="App">add review</div>;
+}
+
+export default AddReview;
---
+++ frontend/src/components/restaurants.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Restaurant() {
+  return (
+    <div className="App">
+      restaurant
+    </div>
+  );
+}
+
+export default Restaurant;
