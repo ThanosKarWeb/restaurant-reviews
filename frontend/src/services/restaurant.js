56bdf6976f0e8d134ce365c6856fbd3f33f82795
ThanosKarWeb <thanos.kar.web@gmail.com>
2023-03-07 22:41:38 +0200

    Added specific restaurant page with reviews. Fixed invalid endpoint that used query params instead of path params

--- frontend/src/services/restaurant.js
+++ frontend/src/services/restaurant.js
@@ -2,16 +2,23 @@ import http from "../http-common";
 
 class RestaurantDataService {
   getAll(page = 0) {
-    return http.get(`restaurants?page=${page}`);
+    return http.get(`/restaurants?page=${page}`);
   }
 
   get(id) {
-    return http.get(`/restaurant?id=${id}`);
+    console.log(id);
+    //return http.get(`/restaurant?id=${id}`);
+    //return http.get(`/restaurants/id/5eb3d668b31de5d588f4292a`);
+    return http.get(`/restaurants/id/${id}`);
+  }
+
+  getCuisines(id) {
+    return http.get(`/restaurants/cuisines`);
   }
 
   find(query, by = "name", page = 0) {
-    return http.get(`restaurants?${by}=${query}&page=${page}`);
-  } 
+    return http.get(`/restaurants?${by}=${query}&page=${page}`);
+  }
 
   createReview(data) {
     return http.post("/review-new", data);
@@ -22,13 +29,10 @@ class RestaurantDataService {
   }
 
   deleteReview(id, userId) {
-    return http.delete(`/review-delete?id=${id}`, {data:{user_id: userId}});
+    return http.delete(`/review-delete?id=${id}`, {
+      data: { user_id: userId },
+    });
   }
-
-  getCuisines(id) {
-    return http.get(`restaurants/cuisines`);
-  }
-
 }
 
 export default new RestaurantDataService();
